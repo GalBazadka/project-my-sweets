437ea80189f1a3bab3c19e0c9c919becded4dc62
GalBazadka <galbazadka@gmail.com>
2022-06-14 23:07:50 +0300

    Update pictureSlide.js

--- scripts/pictureSlide.js
+++ scripts/pictureSlide.js
@@ -1,15 +1,14 @@
-let myIndex = 0;
+let imgCounter = 0;
 carousel();
 function carousel() {
-    let i;
-    const x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
+    const images = document.getElementsByClassName("mySlides");
+    for (let i = 0 ; i < images.length; i++) {
+        images[i].style.display = "none";
     }
-        myIndex++;
-        if (myIndex > x.length) {
-            myIndex = 1
+        imgCounter++;
+        if (imgCounter > images.length) {
+            imgCounter = 1;
         }
-    x[myIndex-1].style.display = "block";
+    images[imgCounter-1].style.display = "block";
     setTimeout(carousel, 3000); // Change image every 2 seconds
 }
