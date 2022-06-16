7dc9b92680a043356454e255e44942f2f1e7f8bf
GalBazadka <galbazadka@gmail.com>
2022-06-16 08:55:59 +0300

    Update menu.js

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,8 +1,13 @@
 function openMenu() {
     const menu = document.getElementById("myLinks");
+    const hamburger = document.getElementsByClassName("hamburgerImg");
     if (menu.style.display === "block") {
         menu.style.display = "none";
     } else {
         menu.style.display = "block";
+        hamburger.style.position = "absolute";
+        hamburger.style.top = "50px";
+        document.getElementsByClassName("hamburgerImg").style.position = "absolute";
+        document.getElementsByClassName("hamburgerImg").style.top = "-150px";
     }
 }
