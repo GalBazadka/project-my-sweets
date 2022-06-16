803ec69f141fabf26290b11e894828f066c4765e
GalBazadka <galbazadka@gmail.com>
2022-06-16 08:56:02 +0300

    Update pictureSlide.js

--- scripts/pictureSlide.js
+++ scripts/pictureSlide.js
@@ -12,3 +12,19 @@ function carousel() {
     images[imgCounter-1].style.display = "block";
     setTimeout(carousel, 3000); // Change image every 2 seconds
 }
+
+imgCounter = 0;
+carousel();
+function carousel() {
+    const images = document.getElementsByClassName("mySlidesSmall");
+    for (let i = 0 ; i < images.length; i++) {
+        images[i].style.display = "none";
+    }
+        imgCounter++;
+        if (imgCounter > images.length) {
+            imgCounter = 1;
+        }
+    images[imgCounter-1].style.display = "block";
+    setTimeout(carousel, 3000); // Change image every 2 seconds
+}
+
