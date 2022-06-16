5b4368024c88ebde490c35b326c9e41a360fa4bb
GalBazadka <galbazadka@gmail.com>
2022-06-16 09:09:55 +0300

    Create ‏‏pictureSlideSmall.js

---
+++ scripts/‏‏pictureSlideSmall.js
@@ -0,0 +1,15 @@
+let imgCounter = 0;
+carousel();
+function carousel() {
+    const images = document.getElementsByClassName("mySlidesSmall");
+    for (let i = 0 ; i < images.length; i++) {
+        images[i].style.display = "none";
+    }
+        imgCounter++;
+        if (imgCounter > images.length) {
+            imgCounter = 1;
+        }
+    images[imgCounter-1].style.display = "block";
+    setTimeout(carousel, 3000); // Change image every 2 seconds
+}
+
